4b7c963e9fff1b941a0c136d0c2f2c8b03cc7da2
Mikhail Korsun <mr.reduction@gmail.com>
2023-02-23 21:51:03 +0300

    Create Task8

--- Task6/Program.cs
+++ Task6/Program.cs
@@ -10,4 +10,4 @@ int firstNumber = Convert.ToInt32(Console.ReadLine());
 if (firstNumber % 2 == 0)
     Console.WriteLine("Число четное ");
 else
-        Console.WriteLine("Число нечетное ");
+    Console.WriteLine("Число нечетное ");
---
+++ Task8/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+Console.Write("Четные числа:");
+while (index <= number)
+{
+    Console.Write($" {index}");
+    index+=2;
+}
